5c15dc577cbab879fdfc468506e92a6e0f1c0fad
Oren <134296255+orenfackrell@users.noreply.github.com>
2023-07-09 00:55:53 +0100

    Have buttons with alternating icons and styles

--- scripts.js
+++ scripts.js
@@ -1,22 +1,12 @@
-const buttons = document.querySelectorAll('.lock-button');
+const lockButtons = document.querySelectorAll('.lock-button');
 
-buttons.forEach(function(button) {
-  const imageElement = button.querySelector('.lock-status');
+lockButtons.forEach((button) => {
+  const img = button.querySelector('.lock-status');
 
-  button.addEventListener('click', function() {
-    console.log(button.textContent)
-    console.log(imageElement.src)
+  button.addEventListener('click', () => {
+    const isOpen = img.src.includes('lock-key-open.svg');
+    img.src = isOpen ? 'assets/icons/lock-key.svg' : 'assets/icons/lock-key-open.svg';
 
-    if (button.textContent == 'Lock') {
-        button.textContent = 'Unlock';
-      imageElement.src = 'assets/icons/lock-key.svg';
-    } else if (button.textContent == 'Unlock'){
-        button.textContent = 'Lock';
-      imageElement.src = 'assets/icons/lock-key-open.svg';
-    }
+    button.classList.toggle('open');
   });
 });
-
-// console log is telling me that both the image and text are updating as desired but the live server is making the image 
-// vanish when the text updates and the text stays the same when only the image updates. Committing at this stage to see if
-// the regular browser works differently.
